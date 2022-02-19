45741adc850ca3ae70bb1b65393561e3e257d196
ongarrick <ong3@duke.edu>
2022-02-19 17:45:53 -0500

    deleted all folders except asset

--- Frigid/Assets/Scripts/PlayerMovement.cs
+++ Frigid/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
     //name of player object
     private Rigidbody2D body; 
+
+    //serialize feild makes variable editable from Unity 
+    [SerializeField] private float speed; 
 
     //starts up when game is loaded
     private void Awake() {
@@ -14,8 +18,21 @@ public class PlayerMovement : MonoBehaviour
 
     //updates position state for for every frame of game
     private void Update() {
-        //first coordinate is left and right movement
-        //second designates no upward movement
-        body.velocity = new Vector2(Input.GetAxis("Horizontal"), body.velocity.y);
+        
+        //gets left and right keystrokes
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        //makes character move left and right, no upwards(y) movement
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //makes character "flip" to face left and right by transforming x scale b/t 1 and -1
+        if(horizontalInput > 0.01f) 
+            transform.localScale = Vector3.one * 6;
+        else if(horizontalInput < -0.01f) 
+            transform.localScale = new Vector3(-6, 6, 6);        
+
+        //checks if spacebar was pressed to make character jump
+        if(Input.GetKey(KeyCode.W))
+            body.velocity = new Vector2(body.velocity.x, speed);
     }
 }
