0585527361de20977795e46aa8b88d95ec6008b5
ongarrick <ong3@duke.edu>
2022-02-19 20:09:04 -0500

    added character flipping to face different directions mechanic, added asset pack as placeholders

--- Frigid/Assets/Scripts/PlayerMovement.cs
+++ Frigid/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     //name of player object
     private Rigidbody2D body; 
+    [SerializeField] private float speed;
 
     //starts up when game is loaded
     private void Awake() {
@@ -14,8 +15,27 @@ public class PlayerMovement : MonoBehaviour
 
     //updates position state for for every frame of game
     private void Update() {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+
         //first coordinate is left and right movement
-        //second designates no upward movement
-        body.velocity = new Vector2(Input.GetAxis("Horizontal"), body.velocity.y);
+        //second designates no upward(y) movement
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        //makes character jump if "w" key pressed
+        if(Input.GetKey(KeyCode.W)) 
+            body.velocity = new Vector2(body.velocity.x, speed);
+
+        //flip player to face left and right
+        if (horizontalInput > 0.01f) 
+            transform.localScale = Vector3.one * 5;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-5, 5, 5);
+            
     }
+
+
+
+    
+
 }
